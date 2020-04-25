32cec47b1df29d4a27801dd9a8ed52238bb134ac
cjs <942403648@qq.com>
2020-04-26 00:12:11 +0800

    用户退出登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container">
+  <div class="container ">
     <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
       @if (Auth::check())
