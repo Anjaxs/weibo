0a5c1148708c5754fbcede67382e3c39d4d56a64
cjs <942403648@qq.com>
2020-04-28 23:57:32 +0800

    关注按钮

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::get('users/{user}/followings', 'UsersController@followings')->name('users
 Route::get('users/{user}/followers', 'UsersController@followers')->name('users.followers');
 Route::resource('users', 'UsersController');
 
+Route::post('users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
+
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
