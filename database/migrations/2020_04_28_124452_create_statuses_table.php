b8bfc7a8009f55188cf9923d7188aa839b3b6ec8
cjs <942403648@qq.com>
2020-04-28 22:36:20 +0800

    微博数据模型

--- database/migrations/2020_04_28_124452_create_statuses_table.php
+++ database/migrations/2020_04_28_124452_create_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class CreateStatusesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('statuses', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->text('content');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->index(['created_at']);
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('statuses');
     }
 }
