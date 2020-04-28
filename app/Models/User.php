e1b33013154751c1a3eb005d096a85371523ac28
cjs <942403648@qq.com>
2020-04-29 00:00:52 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,16 +78,17 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
-
-
-    /******************* 关联模型 *******************/
-
     // 获取动态流
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                     ->with('user')
+                     ->orderBy('created_at', 'desc');
     }
+
+    /******************* 关联模型 *******************/
 
     // 获取微博
     public function statuses()
